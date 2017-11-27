f8a951e0ee93cff84b243b17de7f5c13bc7dfe2f
Pedro Paiva <pedrovsp109@gmail.com>
2017-11-27 23:04:56 +0000

    Seção 2 Aula 17 Model Admin

--- simplemooc/core/admin.py
+++ simplemooc/core/admin.py
@@ -4,4 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from simplemooc.courses.models import Course
 
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    
+    list_display = ['name', 'slug', 'start_date', 'created_at']
+    search_fields = ['name', 'slug']
+
+
+admin.site.register(Course, CourseAdmin)
