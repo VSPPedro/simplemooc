cf39d3e811b1c3e78a5c69559b56866d41d3758f
Pedro Paiva <pedrovsp109@gmail.com>
2017-11-28 16:46:59 +0000

    Seção 3 Aula 22 Página do Curso

--- simplemooc/courses/urls.py
+++ simplemooc/courses/urls.py
@@ -4,4 +4,5 @@ from simplemooc.courses import views as courses_views
 
 urlpatterns = [
     url(r'^$', courses_views.index, name='index'),
+    url(r'^(?P<pk>\d+)', courses_views.details, name='details'),
 ]
