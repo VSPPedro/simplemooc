40524fe7cce33ff67de16048430d5b5a240c38c2
Pedro Paiva <pedrovsp109@gmail.com>
2017-12-05 14:24:00 +0000

    Seção 3 Aula 28 Submetendo o Form do Curso

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -18,16 +18,18 @@ def index(request):
     
 def details(request, slug):
     course = get_object_or_404(Course, slug=slug)
+    context = {}
+    
     if request.method == 'POST':
         form = ContactCoure(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            form = ContactCoure()
     else:
         form = ContactCoure()
     
-    context = {
-        'course': course,
-        'form': form
-    }
-    
+    context['form'] = form
+    context['course'] = course 
     template_name = 'courses/details.html'
     
     return render(request, template_name, context)
